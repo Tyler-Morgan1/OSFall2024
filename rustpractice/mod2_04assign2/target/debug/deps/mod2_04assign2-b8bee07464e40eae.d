859f686a7c45e764eb8e76978be3823eadebd5aa
Tyler Morgan <101295311+Tyler-Morgan1@users.noreply.github.com>
2024-10-07 17:59:32 +0000

    initial set up for mod2_04_assign2

---
+++ rustpractice/mod2_04assign2/target/debug/deps/mod2_04assign2-b8bee07464e40eae.d
@@ -0,0 +1,5 @@
+/workspaces/OSFall2024/rustpractice/mod2_04assign2/target/debug/deps/mod2_04assign2-b8bee07464e40eae: src/main.rs
+
+/workspaces/OSFall2024/rustpractice/mod2_04assign2/target/debug/deps/mod2_04assign2-b8bee07464e40eae.d: src/main.rs
+
+src/main.rs:
